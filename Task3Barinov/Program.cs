c3aaeafc90ad730c063985484ae42ea977b8f5a3
Марсель Баринов <barinov.marsel1111@gmail.com>
2023-05-13 14:35:01 +0300

    Добавьте файлы проекта.

---
+++ Task3Barinov/Program.cs
@@ -0,0 +1,19 @@
+﻿class TextFileWriter
+{
+    static void Main(string[] args)
+    {
+        TextWriter tw = new StreamWriter("date.txt");
+        Console.WriteLine("Документ создан");
+        tw.WriteLine("Марсель" + " " + "Практика" + " " +  "Task3");
+        Console.WriteLine("Дата сегодняшняя записана");
+        tw.Close();
+        Console.WriteLine("Документ закрыт");
+        TextReader tr = new StreamReader("date.txt");
+        Console.WriteLine("Документ открыт");
+        Console.WriteLine("Содержимое документа");
+        Console.WriteLine(tr.ReadLine());
+        tr.Close();
+        Console.WriteLine("Документ закрыт");
+        
+    }
+}
